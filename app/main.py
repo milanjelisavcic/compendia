5016bd588dca566557581cb8223d29aa04108de9
Milan Jelisavcic <294258+milanjelisavcic@users.noreply.github.com>
2021-06-27 21:07:00 +0200

    Add pdf2docx converter to main

--- app/main.py
+++ app/main.py
@@ -4,12 +4,22 @@ from flask import render_template
 from flask import request
 from flask_cors import CORS
 
+from pdf2docx import Converter
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def root():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        cv = Converter(file)
+        cv.convert(f'res/{file}.docx')
+        cv.close()
+
     return "TBA"
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
